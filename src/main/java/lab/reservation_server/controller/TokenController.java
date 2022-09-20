0ad185131d051346ee47e93ee78836591cd64ee4
soochan <tncksdl05@gmail.com>
2022-09-21 02:16:14 +0900

    refactor : 토큰 응답형식 간소화
    트큰 응답 형식을 간소화 status, message는 남기지 않기로 결정

--- src/main/java/lab/reservation_server/controller/TokenController.java
+++ src/main/java/lab/reservation_server/controller/TokenController.java
@@ -5,14 +5,12 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import javax.validation.Valid;
 import lab.reservation_server.dto.request.ExpireDate;
-import lab.reservation_server.dto.response.DefaultDataResponse;
 import lab.reservation_server.dto.response.token.TokenValue;
 import lab.reservation_server.service.TokenService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,13 +29,14 @@ public class TokenController {
      *
      * @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
      */
-    @GetMapping("/api/token")
+    @PostMapping("/api/token")
     @ApiOperation(value="토큰 발급" , notes = "유효기간이 존재하는 토큰을 받을 수 있다.")
-    public ResponseEntity<DefaultDataResponse<TokenValue>> createToken(@RequestBody @Valid ExpireDate expirationDate) {
+    public ResponseEntity<TokenValue> createToken(@RequestBody @Valid ExpireDate expirationDate) {
 
       String token = tokenService.createToken(expirationDate);
       TokenValue tokenValue = new TokenValue(token);
-      return ResponseEntity.ok(DefaultDataResponse.of(HttpStatus.CREATED,"success for token", tokenValue));
+
+      return ResponseEntity.ok(tokenValue);
     }
 
 
