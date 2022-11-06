aed6b004a659835abbbce917e0102b1bfa1e1d4c
soochan <tncksdl05@gmail.com>
2022-11-06 20:42:02 +0900

    feat : add report entity
    문의 신고 entity 추가

---
+++ src/main/java/lab/reservation_server/domain/Report.java
@@ -0,0 +1,28 @@
+package lab.reservation_server.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 문의 신고
+ */
+@Entity
+@Getter
+@NoArgsConstructor
+public class Report {
+
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private Long id;
+
+   @Column(nullable = false)
+   private String title;
+
+   @Column(nullable = false)
+   private String content;
+}
